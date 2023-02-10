ea72966bbe7e6191ea73b8942f2ec8c12a7c1014
prosaicPoet <thomaso'brien@hollandandbarrett.com>
2023-02-10 13:47:32 +0000

    feat: refactor old, and add new answers

--- chapter4.js
+++ chapter4.js
@@ -1,10 +1,34 @@
-// 4.2 Write a recursive function to count the number of items in a list
-const getLength = (arr) => {
-  const firstEl = arr.shift()
-  if (firstEl) {
-    let count = listCount(arr)
-    return (count += 1)
+// 4.1 Write a recursive function to sum all the numbers in a list
+const sum = (arr) => {
+  const el = arr[0]
+  if (el) {
+    return sum(arr.slice(1)) + el
   } else {
     return 0
   }
 }
+
+// 4.2 Write a recursive function to count the number of items in a list
+const length = (arr) => {
+  const el = arr[0]
+  if (el) {
+    let count = length(arr.slice(1))
+    return count += 1
+  } else {
+    return 0
+  }
+}
+
+// 4.3 Find the maximum number in a list
+const max = (arr) => {
+  const el = arr[0]
+  if (el) {
+    const int = max(arr.slice(1))
+    return int > el ? int : el
+  } else {
+    return 0
+  }
+}
+
+// 4.4 Binary search
+const find = (arr, val) => {}
